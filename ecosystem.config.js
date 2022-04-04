9353b3b9a1ec797b716b815b716105431c6c5fb3
Tran Minh Hieu <tranhieu23.code@outlook.com>
2022-04-04 21:55:57 +0800

    initial commit

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    apps: [
+        {
+            name: "export_service_grpc_server",
+            script: "./dist/main.js",
+            args: "--start_grpc_server",
+            instances: 2,
+            instance_var: "NODE_ID",
+        },
+        {
+            name: "export_service_kafka_consumer",
+            script: "./dist/main.js",
+            args: "--start_kafka_consumer",
+            instances: 1,
+            instance_var: "NODE_ID",
+            wait_ready: true,
+        },
+    ],
+};
