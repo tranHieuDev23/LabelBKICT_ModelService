81a5aeabe151621dea41dcff599e4c9b2cc2b681
Trần Minh Hiếu <tranhieudev23@Trans-MacBook-Pro.local>
2023-07-19 16:44:42 +0700

    Update migration scrips

--- migrations/20230709052149_update_time.ts
+++ migrations/20230709052149_update_time.ts
@@ -4,7 +4,7 @@ const TabNameModelServiceDetectionTask = "model_service_detection_task_tab";
 
 export async function up(knex: Knex): Promise<void> {
     await knex.schema.alterTable(TabNameModelServiceDetectionTask, (tab) => {
-        tab.bigInteger("update_time").notNullable();
+        tab.bigInteger("update_time").notNullable().defaultTo(0);
         tab.index(["request_time"], "model_service_detection_task_request_time_idx");
         tab.index(["update_time"], "model_service_detection_task_update_time_idx");
     });
