caf9c36fef16dc58ec7a7c9667ac4599e59032cb
Trần Minh Hiếu <tranhieudev23@Trans-MacBook-Pro.local>
2023-07-20 22:39:00 +0700

    Update cron.d

--- deploy/cron.d
+++ deploy/cron.d
@@ -1,4 +1,4 @@
 # This command helps update detection tasks that are stuck in PROCESSING status after a certain amount of time back to REQUESTED status.
-* * * * * node /build/dist/main.js --update_processing_detection_task_to_requested
+* * * * * /usr/local/bin/node /build/dist/main.js --update_processing_detection_task_to_requested
 # This command triggers the manual processing of REQUESTED detection tasks
-*/30 * * * * node /build/dist/main.js --process_requested_detection_task
+*/30 * * * * /usr/local/bin/node /build/dist/main.js --process_requested_detection_task
